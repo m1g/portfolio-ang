673da9b8b5420832d935577d180639b800f855db
m1g <miguelmalcolm@outlook.com>
2017-06-27 15:58:40 -0400

    updated sticky footer

--- src/app/sections/portfolio/portfolio.component.ts
+++ src/app/sections/portfolio/portfolio.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PortfolioComponent implements OnInit {
 
   public projects: Array<Object> = [
-    { name: 'USF - My Compass', caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam distinctio illo soluta sed ipsam impedit corporis architecto, et dicta odit saepe!', image: '../../assets/usf.jpg', link:'' },
+    { name: 'USF - My Compass', caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam distinctio illo soluta sed ipsam impedit corporis architecto, et dicta odit saepe!', image: '../../assets/usf.jpg', link:'http://usfcompass.swdlab.com/usf/' },
     { name: 'Project 2', caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam distinctio illo soluta sed ipsam impedit corporis architecto, et dicta odit saepe!', image: 'http://placehold.it/700x400', link:'' },
     { name: 'Project 3', caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam distinctio illo soluta sed ipsam impedit corporis architecto, et dicta odit saepe!', image: 'http://placehold.it/700x400', link:'' },
     { name: 'Project 4', caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam distinctio illo soluta sed ipsam impedit corporis architecto, et dicta odit saepe!', image: 'http://placehold.it/700x400', link:'' },
