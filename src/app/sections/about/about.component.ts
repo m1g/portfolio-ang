293d7ce4c8a6a885bbbaef2eb3d696dbc70568f1
m1g <miguelmalcolm@outlook.com>
2017-06-29 12:41:24 -0400

    started adding icons to about page

--- src/app/sections/about/about.component.ts
+++ src/app/sections/about/about.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  public icons = [
+    { src: '../../assets/ang.png', link: '#', alt: 'angular' },
+    { src: '../../assets/react.png', link: '#', alt: 'react' },
+    { src: '../../assets/js.jpg', link: '#', alt: 'javascript' },
+    { src: '../../assets/ts.png', link: '#', alt: 'typescript' }
+  ];
+  
 
   constructor() { }
 
