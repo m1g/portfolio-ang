4e19f1f18bcc33ca79b801344d7d4fedc88fb551
m1g <miguelmalcolm@outlook.com>
2017-07-12 18:38:46 -0400

    updated main project image

--- src/app/projects.service.ts
+++ src/app/projects.service.ts
@@ -1,6 +1,6 @@
 export class ProjectsService {
   projects = [
-    { name: 'USF - My Compass', caption: 'Using the Angular CLI (Angular JS and Typescript), this tool was built for the local Sarasota/Manatee campus', image: '../../assets/usf.jpg', link:'https://gist.github.com/m1g/06cdc048da09f40536a9a7550beb3f05' },
+    { name: 'USF - My Compass', caption: 'Using the Angular CLI (Angular JS and Typescript), this tool was built for the local Sarasota/Manatee campus', image: '../../assets/usf.png', link:'https://gist.github.com/m1g/06cdc048da09f40536a9a7550beb3f05' },
     { name: 'Project 2', caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam distinctio illo soluta sed ipsam impedit corporis architecto, et dicta odit saepe!', image: 'http://placehold.it/700x400', link:'' },
     { name: 'Project 3', caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam distinctio illo soluta sed ipsam impedit corporis architecto, et dicta odit saepe!', image: 'http://placehold.it/700x400', link:'' },
     { name: 'Project 4', caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam distinctio illo soluta sed ipsam impedit corporis architecto, et dicta odit saepe!', image: 'http://placehold.it/700x400', link:'' },
