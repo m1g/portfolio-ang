de0d0d22d649fbfbdc85af3164e3ab2c6a578cb5
m1g <miguelmalcolm@outlook.com>
2017-07-19 14:10:25 -0400

    removed contact page. added fontawesome. completed about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { PortfolioComponent } from './sections/portfolio/portfolio.component';
 import { AboutComponent } from './sections/about/about.component';
 import { BlogComponent } from './sections/blog/blog.component';
-import { ContactComponent } from './sections/contact/contact.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/portfolio', pathMatch: 'full'},
   {path: 'portfolio', pathMatch: 'full', component: PortfolioComponent },
   {path: 'about', pathMatch: 'full', component: AboutComponent },
-  {path: 'blog', pathMatch: 'full', component: BlogComponent },
-  {path: 'contact', pathMatch: 'full', component: ContactComponent }
+  {path: 'blog', pathMatch: 'full', component: BlogComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { PortfolioComponent } from './sections/portfolio/portfolio.component';
 import { AboutComponent } from './sections/about/about.component';
 import { BlogComponent } from './sections/blog/blog.component';
-import { ContactComponent } from './sections/contact/contact.component';
+
+import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     PortfolioComponent,
     AboutComponent,
-    BlogComponent,
-    ContactComponent
+    BlogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
