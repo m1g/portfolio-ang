7d8f8d737e4cf92bdfdf4a8ea99c08eb5bb081d1
m1g <miguelmalcolm@outlook.com>
2017-09-14 17:35:53 -0400

    added new project and revised profile

--- src/app/projects.service.ts
+++ src/app/projects.service.ts
@@ -1,7 +1,7 @@
 export class ProjectsService {
   projects = [
     { name: 'USF - My Compass', caption: 'Using the Angular CLI (Angular JS and Typescript), this tool was built for the local Sarasota/Manatee campus', image: '../../assets/usf.png', link:'https://gist.github.com/m1g/06cdc048da09f40536a9a7550beb3f05' },
-    { name: 'Project 2', caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam distinctio illo soluta sed ipsam impedit corporis architecto, et dicta odit saepe!', image: 'http://placehold.it/700x400', link:'' },
+    { name: 'Tic-React-Toe', caption: 'Working with ReactJS, this is a fun game of Tic-Tac-Toe that changes states and highlights the winning combination. A great way to pass the time', image: '../../assets/tic-tac-toe.png', link:'https://gist.github.com/m1g/e717343db93eec56a8b470e7f4515c5c' },
     { name: 'Project 3', caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam distinctio illo soluta sed ipsam impedit corporis architecto, et dicta odit saepe!', image: 'http://placehold.it/700x400', link:'' },
     { name: 'Project 4', caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam distinctio illo soluta sed ipsam impedit corporis architecto, et dicta odit saepe!', image: 'http://placehold.it/700x400', link:'' },
     { name: 'Project 5', caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam distinctio illo soluta sed ipsam impedit corporis architecto, et dicta odit saepe!', image: 'http://placehold.it/700x400', link:'' },
